e23169a436f2a503716a027af8bb054f004d3ebe
KshitijSrivastava <kshitij127@yahoo.co.in>
2018-01-25 03:00:15 +0530

    Added files

---
+++ ps0/ps0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 14:34:26 2018
+
+@author: hp1
+"""
+import numpy
+
+
+x=int(input(" enter a number X"))
+y=int(input(" enter a number Y"))
+#x**y
+log_x=numpy.log2(x)
+xpowy=x**y;
+
+print("X**y= ",xpowy)
+print("log(x) = ",log_x)
